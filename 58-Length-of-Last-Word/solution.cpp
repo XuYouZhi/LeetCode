4511adb3a4c1b44d90bae5aa08d6aa83ef3b4d0a
lanbing510 <xiaosonglan@gmail.com>
2016-04-29 10:58:51 +0800

    [58. Length of Last Word][Accepted]committed by LeetCode Extension

--- 58-Length-of-Last-Word/solution.cpp
+++ 58-Length-of-Last-Word/solution.cpp
@@ -1,21 +1,48 @@
+// //方法一
+// class Solution {
+// public:
+//     int lengthOfLastWord(string s) {
+//         if(s.size()<1) return 0;
+        
+//         int i=s.size()-1;
+//         while(i>=0&&s[i]==' ') --i; //注意末尾的空格
+        
+//         int count=0;
+//         for(;i>=0;--i){
+//             if(s[i]!=' '){
+//                 count++;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+        
+//         return count;
+//     }
+// };
+
+
+
+
+// 方法二
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        if(s.size()<1) return 0;
+        return lengthOfLastWord(s.c_str());
+    }
+    
+    int lengthOfLastWord(const char* s){
+        int len=0;
         
-        int i=s.size()-1;
-        while(i>=0&&s[i]==' ') --i; //注意末尾的空格
-        
-        int count=0;
-        for(;i>=0;--i){
-            if(s[i]!=' '){
-                count++;
+        while(*s){
+            if(*s++!=' '){  //注意*s++的顺序，同一优先级，是自右向左结合的运算符，等价于*(s++)!=' ' 即先判断t=s++  *t!=' ', 然后再++
+                ++len;
             }
-            else{
-                break;
+            else if(*s&&*s!=' '){
+                len=0;
             }
         }
         
-        return count;
+        return len;
     }
 };
