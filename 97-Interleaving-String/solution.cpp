8e4e7f0201902a5a44085e2d36ae65dc11680336
lanbing510 <xiaosonglan@gmail.com>
2016-05-25 10:03:56 +0800

    [97. Interleaving String][Accepted]committed by LeetCode Extension

---
+++ 97-Interleaving-String/solution.cpp
@@ -0,0 +1,26 @@
+// 代码一 动态规划  
+// f[i][j]表示 s1[0,i]和s2[0,j]匹配 s3[0,i+j]   转移方程：f[i][j]=(s1[i-1]==s3[i+j-1]&&f[i-1][j])||(s2[j-1]==s3[i+j-1]&&f[i][j-1])
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s3.length()!=s1.length()+s2.length()) return false;
+        
+        vector<vector<bool>> f=vector<vector<bool>>(s1.length()+1,vector<bool>(s2.length()+1,true));
+        
+        for(int i=1;i<=s1.length();++i){
+            f[i][0]=(s1[i-1]==s3[i-1])&&f[i-1][0];
+        }
+        
+        for(int j=1;j<=s2.length();++j){
+            f[0][j]=(s2[j-1]==s3[j-1])&&f[0][j-1];
+        }
+        
+        for(int i=1;i<=s1.length();++i){
+            for(int j=1;j<=s2.length();++j){
+                f[i][j]=(s1[i-1]==s3[i+j-1]&&f[i-1][j])||(s2[j-1]==s3[i+j-1]&&f[i][j-1]);
+            }
+        }
+        
+        return f[s1.length()][s2.length()];
+    }
+};
