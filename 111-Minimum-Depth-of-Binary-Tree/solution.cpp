f1f6c44874b615b3415978b55a84e5a1ef37b5f1
lanbing510 <xiaosonglan@gmail.com>
2016-05-27 11:32:14 +0800

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.cpp
+++ 111-Minimum-Depth-of-Binary-Tree/solution.cpp
@@ -18,16 +18,50 @@
 // };
 
 
-// 代码一 递归版 O(n)  O(logn)
+// // 代码一 递归版 O(n)  O(logn)
+// class Solution {
+// public:
+//     int minDepth(TreeNode* root) {
+//         return minDepth(root,false);
+//     }
+    
+//     int minDepth(TreeNode* root, bool hasbrother){
+//         if(root==nullptr) return hasbrother?INT_MAX:0;
+        
+//         return min(minDepth(root->left,root->right!=nullptr),minDepth(root->right,root->left!=nullptr))+1;
+//     }
+// };
+
+
+// 代码二 迭代版 O(n)  O(logn)
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        return minDepth(root,false);
-    }
-    
-    int minDepth(TreeNode* root, bool hasbrother){
-        if(root==nullptr) return hasbrother?INT_MAX:0;
+        if(root==nullptr) return 0;
         
-        return min(minDepth(root->left,root->right!=nullptr),minDepth(root->right,root->left!=nullptr))+1;
+        int result=INT_MAX;
+        
+        stack<pair<TreeNode*,int>> s;
+        s.push(make_pair(root,1));
+        
+        while(!s.empty()){
+            auto node=s.top().first;
+            auto depth=s.top().second;
+            s.pop();
+            
+            if(node->left==nullptr&&node->right==nullptr){ //遍历到叶节点
+                result=min(result,depth);
+            }
+            
+            if(node->left&&result>depth){ // 剪枝
+                s.push(make_pair(node->left,depth+1));
+            }
+            
+            if(node->right&&result>depth){
+                s.push(make_pair(node->right,depth+1));
+            }
+        }
+        
+        return result;
     }
 };
