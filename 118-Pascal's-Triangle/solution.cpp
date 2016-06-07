5f13353fda9d51093c5445d1389d2b2e634ea7fc
lanbing510 <xiaosonglan@gmail.com>
2016-06-07 11:11:51 +0800

    [118. Pascal's Triangle][Accepted]committed by LeetCode Extension

---
+++ 118-Pascal's-Triangle/solution.cpp
@@ -0,0 +1,24 @@
+// 代码一 动态规划  O(n)  O(n)
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        if(numRows<1) return result;
+        
+        vector<int> f(numRows,1);
+        for(int r=0;r<numRows;++r){
+            for(int c=r-1;c>=1;--c){
+                f[c]=f[c-1]+f[c];
+            }
+            
+            result.push_back(vector<int>());
+            for(int i=0;i<=r;++i){
+                result.back().push_back(f[i]);
+                //cout<<f[i]<<" "; 
+            }
+            //cout<<endl;
+        }
+        
+        return result;
+    }
+};
