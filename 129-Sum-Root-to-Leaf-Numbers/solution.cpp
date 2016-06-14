8ab70cfd73fd9ac46337ce0fe32a749ec112d76f
lanbing510 <xiaosonglan@gmail.com>
2016-06-14 16:04:07 +0800

    [129. Sum Root to Leaf Numbers][Accepted]committed by LeetCode Extension

--- 129-Sum-Root-to-Leaf-Numbers/solution.cpp
+++ 129-Sum-Root-to-Leaf-Numbers/solution.cpp
@@ -7,30 +7,50 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// // 代码一 O(n) O(logn)
+// class Solution {
+// public:
+//     int sumNumbers(TreeNode* root) {
+//         int result=0;
+//         if(root==nullptr) return 0;
+//         int sum=0;
+//         sumNumbers(root,result,sum);
+//         return result;
+//     }
+    
+//     void sumNumbers(TreeNode* root, int& result, int sum){
+//         sum+=root->val;
+        
+//         if(root->left==nullptr&&root->right==nullptr){
+//             result+=sum;
+//             return;
+//         }
+        
+//         if(root->left!=nullptr){
+//             sumNumbers(root->left,result,sum*10);
+//         }
+        
+//         if(root->right!=nullptr){
+//             sumNumbers(root->right,result,sum*10);
+//         }
+//     }
+// };
+
+
+// 代码二  精简版  O(n) O(logn)
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
-        int result=0;
-        if(root==nullptr) return 0;
-        int sum=0;
-        sumNumbers(root,result,sum);
-        return result;
+        return dfs(root,0);
     }
     
-    void sumNumbers(TreeNode* root, int& result, int sum){
-        sum+=root->val;
-        
+private:
+    int dfs(TreeNode* root, int sum){
+        if(root==nullptr) return 0;
         if(root->left==nullptr&&root->right==nullptr){
-            result+=sum;
-            return;
+            return sum*10+root->val;
         }
         
-        if(root->left!=nullptr){
-            sumNumbers(root->left,result,sum*10);
-        }
-        
-        if(root->right!=nullptr){
-            sumNumbers(root->right,result,sum*10);
-        }
+        return dfs(root->left,sum*10+root->val)+dfs(root->right,sum*10+root->val);
     }
 };
