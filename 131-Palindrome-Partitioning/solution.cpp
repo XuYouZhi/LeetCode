251af64509cf9b28c921ff77ef18413b71a42ece
lanbing510 <xiaosonglan@gmail.com>
2016-06-15 09:36:34 +0800

    [131. Palindrome Partitioning][Accepted]committed by LeetCode Extension

---
+++ 131-Palindrome-Partitioning/solution.cpp
@@ -0,0 +1,39 @@
+// 代码一 深搜 回溯  长度为n的字符串有n-1个地方可以砍断，每个地方可断可不断，因此复杂度为O(2^n)
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+          vector<vector<string>> result;
+          vector<string> path;
+          dfs(s,path,result,0,1);
+          return result;
+    }
+    
+private:
+    void dfs(string& s, vector<string>& path, vector<vector<string>>& result, int prev, int start){
+        if(start==s.size()){
+            if(isPalindrome(s,prev,start-1)){
+                path.push_back(s.substr(prev,start-prev));
+                result.push_back(path);
+                path.pop_back();
+            }
+            return;
+        }
+        
+        //不断开
+        dfs(s,path,result,prev,start+1);
+        //断开
+        if(isPalindrome(s,prev,start-1)){
+            path.push_back(s.substr(prev,start-prev));
+            dfs(s,path,result,start,start+1);
+            path.pop_back();
+        }
+    }
+    
+    bool isPalindrome(const string &s, int start, int end){
+        while(start<end&&s[start]==s[end]){
+            ++start;
+            --end;
+        }
+        return start>=end;
+    }
+};
