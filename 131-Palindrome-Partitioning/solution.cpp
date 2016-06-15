85469642fb0d64a5ce5ee68301b01488a9168df4
lanbing510 <xiaosonglan@gmail.com>
2016-06-15 10:16:10 +0800

    [131. Palindrome Partitioning][Accepted]committed by LeetCode Extension

--- 131-Palindrome-Partitioning/solution.cpp
+++ 131-Palindrome-Partitioning/solution.cpp
@@ -40,37 +40,74 @@
 
 
 
-// 代码二  另一种写法，更加简洁
+// // 代码二  另一种写法，更加简洁  O(2^n)   O(n)
+// class Solution {
+// public:
+//     vector<vector<string>> partition(string s) {
+//         vector<vector<string>> result;
+//         vector<string> path;
+//         dfs(s,path,result,0);
+//         return result;
+//     }
+    
+//     //搜索以s[start]开头的partition方案
+//     void dfs(string& s, vector<string>& path, vector<vector<string>>& result, int start){
+//         if(start==s.size()){
+//             result.push_back(path);
+//             return;
+//         }
+        
+//         for(int i=start;i<s.size();++i){
+//             if(isPalindrome(s,start,i)){ //从i位置砍一刀
+//                 path.push_back(s.substr(start,i-start+1));
+//                 dfs(s,path,result,i+1);
+//                 path.pop_back();
+//             }
+//         }
+//     }
+    
+//     bool isPalindrome(const string &s, int start, int end){
+//         while(start<end&&s[start]==s[end]){
+//             ++start;
+//             --end;
+//         }
+//         return start>=end;
+//     }
+// };
+
+
+// 代码三  动态规划法
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> path;
-        dfs(s,path,result,0);
-        return result;
-    }
-    
-    //搜索以s[start]开头的partition方案
-    void dfs(string& s, vector<string>& path, vector<vector<string>>& result, int start){
-        if(start==s.size()){
-            result.push_back(path);
-            return;
-        }
-        
-        for(int i=start;i<s.size();++i){
-            if(isPalindrome(s,start,i)){ //从i位置砍一刀
-                path.push_back(s.substr(start,i-start+1));
-                dfs(s,path,result,i+1);
-                path.pop_back();
+        const int n=s.size();
+        bool p[n][n]; //s[i,j]是否回文
+        fill_n(&p[0][0],n*n,false);
+        for(int i=n-1;i>=0;--i){
+            for(int j=i;j<n;++j){
+                p[i][j]=s[i]==s[j]&&((j-i<2)||p[i+1][j-1]);
             }
         }
-    }
-    
-    bool isPalindrome(const string &s, int start, int end){
-        while(start<end&&s[start]==s[end]){
-            ++start;
-            --end;
+        
+        vector<vector<string>> sub_palins[n];
+        
+        for(int i=n-1;i>=0;--i){
+            for(int j=i;j<n;++j){
+                if(p[i][j]){
+                    const string palindrome=s.substr(i,j-i+1);
+                    if(j+1<n){
+                        for(auto v:sub_palins[j+1]){
+                            v.insert(v.begin(),palindrome);
+                            sub_palins[i].push_back(v);
+                        }
+                    }
+                    else{
+                        sub_palins[i].push_back(vector<string>{palindrome});
+                    }
+                }
+            }
         }
-        return start>=end;
+        
+        return sub_palins[0];
     }
 };
