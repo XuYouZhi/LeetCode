e50873475192d160707f2cdf787ccf20c8248733
lanbing510 <xiaosonglan@gmail.com>
2016-06-27 16:30:26 +0800

    [164. Maximum Gap][Accepted]committed by LeetCode Extension

--- 164-Maximum-Gap/solution.cpp
+++ 164-Maximum-Gap/solution.cpp
@@ -1,3 +1,9 @@
+// 桶排序 
+/*
+设从A到B有N个元素  最大的gap不会小于ceiling((B-A)/(N-1))  让一个桶装len=ceiling((B-A)/(N-1))个元素  则最多会有(B-A)/len+1个桶
+将每个元素放入对应的桶中 loc=(K-A)/len  由于每个桶内最大的gap是len-1，则所有元素中最大的gap不会在桶内，只会在桶间
+对每个非空桶p，找到下一个非空q，q.min-p.max可能为答案，遍历后可找到最大的gap
+*/
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
@@ -12,7 +18,7 @@ public:
         
         if(max_num==min_num) return 0;
         
-        int len=ceil((max_num-min_num)/double(n-1)); //(max_num-min_num)/n+1; 
+        int len=ceil((max_num-min_num)/double(n-1)); //(max_num-min_num)/n+1;  (max_num-min_num-1)/(n-1)+1; 不同的ceil方式 
         
         vector<vector<int>> buckets((max_num-min_num)/len+1); //桶的个数
         for(auto i:nums){
