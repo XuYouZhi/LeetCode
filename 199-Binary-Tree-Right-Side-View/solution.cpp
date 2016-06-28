fa97c702658d171882819a3b517f3c9e4bbaf8a5
lanbing510 <xiaosonglan@gmail.com>
2016-06-28 23:21:12 +0800

    [199. Binary Tree Right Side View][Accepted]committed by LeetCode Extension

--- 199-Binary-Tree-Right-Side-View/solution.cpp
+++ 199-Binary-Tree-Right-Side-View/solution.cpp
@@ -25,36 +25,63 @@
 // };
 
 
-// 层级遍历  压入每层的最后一个元素  Breadth-First Search
+// // 代码一 层级遍历  压入每层的最后一个元素  Breadth-First Search
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> result;
+//         if(root==nullptr) return result;
+        
+//         deque<TreeNode*> deque_treenode;
+        
+//         deque_treenode.push_back(root);
+//         int level_size=1;
+//         int level_count=0;
+//         int right;
+//         while(deque_treenode.size()){
+//             TreeNode *node=deque_treenode.front();
+//             deque_treenode.pop_front();
+            
+//             right=node->val;
+//             ++level_count;
+            
+//             if(node->left!=nullptr) deque_treenode.push_back(node->left);
+//             if(node->right!=nullptr) deque_treenode.push_back(node->right);
+            
+//             if(level_count>=level_size){
+//                 result.push_back(right);
+//                 level_count=0;
+//                 level_size=deque_treenode.size();
+//             }
+//         }
+        
+//         return result;
+//     }
+// };
+
+
+
+// 代码二 深度遍历
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> result;
+        int maxdepth=0;
         if(root==nullptr) return result;
         
-        deque<TreeNode*> deque_treenode;
-        
-        deque_treenode.push_back(root);
-        int level_size=1;
-        int level_count=0;
-        int right;
-        while(deque_treenode.size()){
-            TreeNode *node=deque_treenode.front();
-            deque_treenode.pop_front();
-            
-            right=node->val;
-            ++level_count;
-            
-            if(node->left!=nullptr) deque_treenode.push_back(node->left);
-            if(node->right!=nullptr) deque_treenode.push_back(node->right);
-            
-            if(level_count>=level_size){
-                result.push_back(right);
-                level_count=0;
-                level_size=deque_treenode.size();
-            }
-        }
+        rightSideView(root,result,maxdepth,1);
         
         return result;
     }
+    
+    void rightSideView(TreeNode* root, vector<int>& result, int& maxdepth, int depth){
+        if(depth>maxdepth){
+            maxdepth=depth;
+            result.push_back(root->val);
+        }
+        
+        if(root->right!=nullptr) rightSideView(root->right, result, maxdepth, depth+1);
+        if(root->left!=nullptr) rightSideView(root->left, result, maxdepth, depth+1);
+    }
+
 };
