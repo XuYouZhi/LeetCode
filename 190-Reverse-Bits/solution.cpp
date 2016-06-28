d7d6b50987fef192a0a4189168feb9d868a8e928
lanbing510 <xiaosonglan@gmail.com>
2016-06-28 20:57:02 +0800

    [190. Reverse Bits][Accepted]committed by LeetCode Extension

--- 190-Reverse-Bits/solution.cpp
+++ 190-Reverse-Bits/solution.cpp
@@ -3,15 +3,23 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t res=0;
+        
+        // //方法一
+        // for(int i=0;i<32;++i){
+        //     if(n&1==1){
+        //         res=(res<<1)+1;
+        //     }
+        //     else{
+        //         res<<=1;
+        //     }
+        //     n>>=1;
+        // }
+        
+        // 方法二
         for(int i=0;i<32;++i){
-            if(n&1==1){
-                res=(res<<1)+1;
-            }
-            else{
-                res<<=1;
-            }
-            n>>=1;
+            res|=((n>>i)&1)<<(31-i);
         }
+        
         return res;
     }
 };
