9d6af9be1dbb96435823e64d9d40fa4ede44b057
lanbing510 <xiaosonglan@gmail.com>
2016-06-29 17:12:19 +0800

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.cpp
+++ 207-Course-Schedule/solution.cpp
@@ -1,3 +1,7 @@
+// 代码一  DFS
+/*
+思路：先建立好有向图，然后从第一个门课开始，找其前驱课程，暂时将当前课程标记为已访问，然后对新得到的课程调用DFS递归，直到出现新的课程       已经访问过了（有向图有环），则返回false，没有冲突的话（无环）返回true，然后把标记当前课程无冲突（无环，如果一个课程的前驱课程为该课   程，直接返回无冲突）
+*/
 class Solution {
 private:
     unordered_map<int,vector<int>> course;
@@ -14,23 +18,28 @@ public:
         }
         
         unordered_map<int,vector<int>>::iterator iter;
+        unordered_map<int,int> checked;
+        for(iter=course.begin();iter!=course.end();++iter){
+            checked[(*iter).first]=0;
+        }
+        
         for(iter=course.begin();iter!=course.end();++iter){
             int start=(*iter).first;
-            unordered_set<int> checked;
             if(dfs(start, checked)==false) return false;
         }
         
         return true;
     }
     
-    bool dfs(int start, unordered_set<int> checked){
-        checked.insert(start);
+    bool dfs(int start, unordered_map<int,int> &checked){
+        if(checked[start]==-1) return false;
+        if(checked[start]==1) return true; 
+        
+        checked[start]=-1;
         for(auto s:course[start]){
-            cout<<s<<" "<<start<<endl;
-            if(checked.find(s)!=checked.end()) return false;
             if(dfs(s,checked)==false) return false;
         }
-        
+        checked[start]=1;
         return true;
     }
 };
