a29fca5acb5b4c40baebaec378e17f2c4a8498df
lanbing510 <xiaosonglan@gmail.com>
2016-06-29 10:18:17 +0800

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.cpp
+++ 204-Count-Primes/solution.cpp
@@ -1,50 +1,50 @@
-// 代码一 对每个数判断是否为质数（素数）
-class Solution {
-public:
-    int countPrimes(int n) {
-        int result=0;
-        for(int i=1;i<n;++i){
-            if(isPrime(i)){
-                ++result;
-            }
-        }
-        return result;
-    }
-    
-    bool isPrime(int num){
-        if(num<=1) return false;
-        
-        for(int i=2;i*i<=num;++i){
-            if(num%i==0) return false;
-        }
-        
-        return true;
-    }
-};
-
-
-// // 代码二 筛素数法
+// // 代码一 对每个数判断是否为质数（素数）  Time Limit Exceeded
 // class Solution {
 // public:
 //     int countPrimes(int n) {
-//         vector<bool> isprime(n,true);
-//         isprime[0]=isprime[1]=false;
-        
-//         for(int i=2;i*i<=n;++i){
-//             if(isprime[i]==false) continue;
-//             for(int j=i*i;j<n;j+=i){
-//                 isprime[j]=false;
-//             }
-//         }
-        
-        
 //         int result=0;
-//         for(int i=0;i<n;++i){
-//             if(isprime[i]){
+//         for(int i=1;i<n;++i){
+//             if(isPrime(i)){
 //                 ++result;
 //             }
 //         }
-        
 //         return result;
 //     }
+    
+//     bool isPrime(int num){
+//         if(num<=1) return false;
+        
+//         for(int i=2;i*i<=num;++i){
+//             if(num%i==0) return false;
+//         }
+        
+//         return true;
+//     }
 // };
+
+
+// 代码二 筛素数法
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> isprime(n,true);
+        isprime[0]=isprime[1]=false;
+        
+        for(int i=2;i*i<=n;++i){
+            if(isprime[i]==false) continue;
+            for(int j=i*i;j<n;j+=i){
+                isprime[j]=false;
+            }
+        }
+        
+        
+        int result=0;
+        for(int i=0;i<n;++i){
+            if(isprime[i]){
+                ++result;
+            }
+        }
+        
+        return result;
+    }
+};
