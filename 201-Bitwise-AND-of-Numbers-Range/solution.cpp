3d159392f9372376bc5aef672801f3d2505b0a77
lanbing510 <xiaosonglan@gmail.com>
2016-06-29 08:52:27 +0800

    [201. Bitwise AND of Numbers Range][Accepted]committed by LeetCode Extension

--- 201-Bitwise-AND-of-Numbers-Range/solution.cpp
+++ 201-Bitwise-AND-of-Numbers-Range/solution.cpp
@@ -12,15 +12,31 @@
 // };
 
 
-// 代码二 建立一个32位都是1的mask，然后每次向左移一位，比较m和n是否相同，不同再继续左移一位，直至相同，然后把m和mask相与就是最终结果
+// // 代码二 建立一个32位都是1的mask，然后每次向左移一位，比较m和n是否相同，不同再继续左移一位，直至相同，然后把m和mask相与就是最终结果
+// class Solution {
+// public:
+//     int rangeBitwiseAnd(int m, int n) {
+//         unsigned int msk=~(0x0);
+//         while((m&msk)!=(n&msk)){
+//             msk<<=1;
+//         }
+        
+//         return m&msk;
+//     }
+// };
+
+
+// 代码三 直接平移m和n，每次向右移一位，直到m和n相等，记录下所有平移的次数i，然后再把m左移i位即为最终结果
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
-        unsigned int msk=~(0x0);
-        while((m&msk)!=(n&msk)){
-            msk<<=1;
+        int i=0;
+        while(m!=n){
+            m>>=1;
+            n>>=1;
+            ++i;
         }
         
-        return m&msk;
+        return m<<i;
     }
 };
